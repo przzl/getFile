18cf97922c6961dec3e96a91d9e701988ea6fd80
przzl <474988730@qq.com>
2020-08-20 09:54:29 +0800

    11

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ipadToken: '**********', // wechaty的ipad token
+    commonBusUrl: '*************', //获取文件的业务接口地址
+    sendMegType:{"friend":"1","rom":"2"},//消息类型:好友和群
+    roomName:'世界那么大，眼睛那么小'
+}
